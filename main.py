cf923be9166fb5900d7b2cf1001ce3c5c10c8a09
baitian163 <45765738+baitian163@users.noreply.github.com>
2026-02-04 10:28:21 +0800

    Configure TensorFlow to use CPU only
    Disable GPU usage and TensorFlow logging.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 """The central program that ties all the modules together."""
 
 import time
+import os
 from src.modules.bot import Bot
 from src.modules.capture import Capture
 from src.modules.notifier import Notifier
 from src.modules.listener import Listener
 from src.modules.gui import GUI
+
+
+# 禁用所有 GPU 设备，强制 TensorFlow 只看得到 CPU
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+# 禁用 TensorFlow 的日志输出（可选，减少终端噪音）
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 bot = Bot()
